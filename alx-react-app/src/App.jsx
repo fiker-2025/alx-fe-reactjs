f7fb38ff5ce326a620e38b066ffb2bbf679cd4b9
fiker-2025 <fikerteamenu9@gmail.com>
2025-11-07 13:10:35 +0300

    userprofile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,12 +1,27 @@
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import WelcomeMessage from './components/WelcomeMessage';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <>
+      <WelcomeMessage />
       <Header />
       <MainContent />
+      
+      <UserProfile 
+        name="Alice" 
+        age={28} 
+        bio="I love traveling and coding." 
+      />
+      <UserProfile 
+        name="Bob" 
+        age={32} 
+        bio="I enjoy photography and coffee." 
+      />
+
       <Footer />
     </>
   );
